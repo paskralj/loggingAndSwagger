a20bd9a6d6ec3075008e1de05ecdff47283700a6
Tomo <suba213@hotmail.com>
2024-08-05 01:18:30 +0200

    Zadnji commit

--- src/main/java/com/example/loggingzi/demo/service/LogService.java
+++ src/main/java/com/example/loggingzi/demo/service/LogService.java
@@ -1,21 +1,28 @@
 package com.example.loggingzi.demo.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class LogService {
     private static final Logger logger = LoggerFactory.getLogger(LogService.class);
+
+    private final MeterRegistry meterRegistry;
+    public LogService(MeterRegistry meterRegistry){
+        this.meterRegistry=meterRegistry;
+    }
 
     public void logDifferentLevels(){
         logger.info("Info poruka ! ");
         logger.debug("Debug poruka! ");
         logger.warn("Warn poruka! ");
         logger.error("Error jebiga! ");
+
+        meterRegistry.counter("log.level.info").increment();
+        meterRegistry.counter("log.level.debug").increment();
+        meterRegistry.counter("log.level.warn").increment();
+        meterRegistry.counter("log.level.error").increment();
     }
 }
